bc63932660b71b129aebc39c9dca2def89409fd6
pongpatapee <pongpatapee1021@gmail.com>
2024-06-17 23:02:21 -0400

    feat: added delete button, endpoint, and logic

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -26,15 +28,20 @@ type Count struct {
 	Count int
 }
 
+var id int = 0
+
 type Contact struct {
 	Name  string
 	Email string
+	Id    int
 }
 
 func newContract(name string, email string) Contact {
+	id++
 	return Contact{
 		Name:  name,
 		Email: email,
+		Id:    id,
 	}
 }
 
@@ -51,6 +58,16 @@ func newData() Data {
 			newContract("Sugon", "sugondeeznuts@email.com"),
 		},
 	}
+}
+
+func (d *Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+
+	return -1
 }
 
 func (d *Data) hasEmail(email string) bool {
@@ -118,6 +135,28 @@ func main() {
 		return c.Render(200, "oob-contact", contact)
 		// return c.Render(200, "display", page.Data)
 	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(400, "Invalid Id")
+		}
+
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(404, "Contact not found")
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+		time.Sleep(3 * time.Second)
+
+		return c.NoContent(200)
+	})
+
+	e.Static("/images", "images")
+	e.Static("/css", "css")
 
 	e.Logger.Fatal(e.Start(":42069"))
 }
